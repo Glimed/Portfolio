bfcac893e47196730af48569cf55c5e762b2e676
Glimed <schullarj@proton.me>
2025-06-11 13:30:06 -0400

    hmm

---
+++ script.js
@@ -0,0 +1,21 @@
+fun = 5;
+
+function funFunction() {
+    fun = "hello";
+    console.log(1, fun);
+}
+
+function funerFunction() {
+    fun = "bye"
+    console.log(2, fun);
+}
+
+function funestFunction() {
+    fun = "yooooo"
+    console.log(3, fun);
+}
+
+console.log(fun);
+funFunction();
+funerFunction();
+funestFunction();
